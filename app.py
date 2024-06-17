10540de2637a627ba5abb4e81817330cb57ec3ab
shashankpulijala <shashankpulijala@gmail.com>
2024-06-17 09:45:50 +0100

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from transformers import pipeline
 
-rom flask import Flask, render_template, request
+from flask import Flask, render_template, request
 from transformers import pipeline
 
 app = Flask(__name__)
